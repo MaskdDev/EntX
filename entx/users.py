f25b8010ea69678df4f9c9152247722f38649631
MaskdDev <maskddev@gmail.com>
2023-03-02 11:40:12 +1100

    Patch 3.0.6

--- entx/users.py
+++ entx/users.py
@@ -11,27 +11,18 @@ class User:
         else:
             self.user_dir = f"{username}.json"
         self.client = JSONClient(password)
-        self.full_obj = {
-            
-        }
-        self.obj = {
-            
-        }
+        self.obj = {}
     
     @classmethod
     def new_user(cls, username: str, password: str, user_dir: str = None):
         if f"{username}.json" not in os.listdir(user_dir):
             user = cls(username, password, user_dir)
             user_obj = {
-                "username": username,
-                "data": {
-                    
-                }
+                "username": user.username,
+                "data": user.obj
             }
             with open(user.user_dir, "w") as user_file:
                 user.client.dump(user_obj, user_file)
-                user.full_obj = user_obj
-                user.obj = user.full_obj["data"]
             return user
         else:
             raise UserAlreadyExists(f"A user named {username} already exists in this directory.")
@@ -45,16 +36,19 @@ class User:
                 if "username" not in user_obj:
                     raise InvalidPasswordException("Incorrect password provided.")
                 elif user_obj["username"] == username:
-                        user.full_obj = user_obj
-                        user.obj = user.full_obj["data"]
-                        return user
+                    user.obj = user_obj["data"]
+                    return user
                 else:
                     raise InvalidPasswordException("Incorrect password provided.")
         else:
             raise InvalidUser(f"A user named {username} does not exist in this directory.")
         
     def update(self):
+        user_obj = {
+            "username": self.username,
+            "data": self.obj
+        }
         with open(self.user_dir, "w") as user_file:
-            self.client.dump(self.full_obj, user_file)
+            self.client.dump(user_obj, user_file)
     
     
